1ac3bf6ed1cf6f8455b4723112123cae705adfe6
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2022-03-29 09:06:50 -0300

    Estrutura

---
+++ src/Calculadora/Funcoes.java
@@ -0,0 +1,18 @@
+package Calculadora;
+public class Funcoes { 
+    public double Soma(double num1, double num2){
+        return(num1+num2);
+    }
+    
+    public double Subtracao(double num1, double num2){
+        return(num1-num2);
+    }
+    
+    public double Divisao(double num1, double num2){
+        return(num1/num2);
+    }
+    
+    public double Multiplicacao(double num1, double num2){
+        return(num1*num2);
+    }
+}
